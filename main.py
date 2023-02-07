00d32d593bd853bf221fb8e889ed19d7eacc653a
Matthew Casperson <matthewcasperson@gmail.com>
2023-02-07 14:30:42 +1000

    Did diff of steps

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import difflib
 import filecmp
+import json
 import os.path
 import sys
 import tempfile
@@ -35,6 +36,16 @@ def get_args():
                         action='store',
                         help='The project whose releases are compared',
                         required=True)
+    parser.add_argument('--oldRelease',
+                        dest='old_release',
+                        action='store',
+                        help='The previous release to compare',
+                        required=False)
+    parser.add_argument('--newRelease',
+                        dest='new_release',
+                        action='store',
+                        help='The new release to compare',
+                        required=False)
     return parser.parse_args()
 
 
@@ -79,7 +90,7 @@ def project_name_to_id(args, space_id):
 
 
 @retry(stop_max_attempt_number=3, wait_fixed=2000)
-def get_release(args, space_id, project_id, source_release_index=1, destination_release_index=0):
+def get_release(args, space_id, project_id):
     if space_id is None or project_id is None:
         return None
 
@@ -87,15 +98,33 @@ def get_release(args, space_id, project_id, source_release_index=1, destination_
     response = get(url, headers=get_octopus_headers(args))
     response_json = response.json()
 
-    try:
-        return {"source": response_json["Items"][source_release_index],
-                "destination": response_json["Items"][destination_release_index]}
-    except IndexError:
-        sys.stderr.write(
-            "There was an error retrieving the releases at index "
-            + str(source_release_index) + " and "
-            + str(destination_release_index))
+    # We need at least two releases to do a comparison
+    if len(response_json["Items"]) < 2:
         return None
+
+    # Use the two latest releases if none were defined
+    if args.old_release is None or args.new_release is None:
+        return {
+            "source": response_json["Items"][1],
+            "destination": response_json["Items"][0]
+        }
+
+    source = [a for a in response_json["Items"] if a["Version"] == args.old_release]
+    dest = [a for a in response_json["Items"] if a["Version"] == args.new_release]
+
+    if len(source) == 1 and len(dest) == 1:
+        return {
+            "source": source,
+            "destination": dest
+        }
+
+    if len(source) != 1:
+        print("Could not find old release " + args.old_release)
+
+    if len(dest) != 1:
+        print("Could not find new release " + args.new_release)
+
+    return None
 
 
 def get_deployment_process(args, space_id, deployment_process_id):
@@ -103,6 +132,15 @@ def get_deployment_process(args, space_id, deployment_process_id):
         return None
 
     url = args.octopus_url + "/api/" + space_id + "/DeploymentProcesses/" + deployment_process_id
+    response = get(url, headers=get_octopus_headers(args))
+    return response.json()
+
+
+def get_variables(args, space_id, variables_is):
+    if space_id is None or variables_is is None:
+        return None
+
+    url = args.octopus_url + "/api/" + space_id + "/Variables/" + variables_is
     response = get(url, headers=get_octopus_headers(args))
     return response.json()
 
@@ -132,7 +170,16 @@ def package_from_built_in_feed(built_in_feed_id, deployment_process, step_name, 
     return False
 
 
-def get_release_packages(args, built_in_feed_id, space_id, releases):
+def flatten_release_with_packages_and_deployment(args, built_in_feed_id, space_id, releases):
+    """
+    When performing a diff between two release we are interested in:
+    * The packages included in the release
+    * Whether the packages are sourced from the built-in feed (as we can download these packages)
+    * The deployment process snapshotted with the release
+    * The variables snapshotted with the release
+    This function flattens the release, deployment process, variables, and additional information such as
+    the source of the packages into a simple map. This map is what the rest of the script works with.
+    """
     if releases is None or built_in_feed_id is None or space_id is None:
         return None
 
@@ -140,37 +187,44 @@ def get_release_packages(args, built_in_feed_id, space_id, releases):
     for release in releases.keys():
         deployment_process = get_deployment_process(args, space_id,
                                                     releases[release].get("ProjectDeploymentProcessSnapshotId"))
+        variables = get_variables(args, space_id,
+                                                    releases[release].get("ProjectVariableSetSnapshotId"))
         packages = releases[release].get("SelectedPackages")
         if packages is not None:
-            releases_map[release] = [{
-                "id": a.get("PackageReferenceName"),
-                "version": a.get("Version"),
-                "from_built_in_feed": package_from_built_in_feed(built_in_feed_id,
-                                                                 deployment_process,
-                                                                 a.get("StepName"),
-                                                                 a.get("ActionName"),
-                                                                 a.get("PackageReferenceName"))
-            } for a in packages]
+            releases_map[release] = {
+                "packages": [{
+                    "id": a.get("PackageReferenceName"),
+                    "version": a.get("Version"),
+                    "from_built_in_feed": package_from_built_in_feed(built_in_feed_id,
+                                                                     deployment_process,
+                                                                     a.get("StepName"),
+                                                                     a.get("ActionName"),
+                                                                     a.get("PackageReferenceName")),
+                } for a in packages],
+                "deployment_process": deployment_process,
+                "variables": variables,
+                "version": releases[release].get("Version")
+            }
 
     return releases_map
 
 
 def list_package_diff(release_packages, print_new_package, print_removed_package):
-    for package in release_packages["destination"]:
-        if len([a for a in release_packages["source"] if a["id"] == package["id"]]) == 0:
+    for package in release_packages["destination"]["packages"]:
+        if len([a for a in release_packages["source"]["packages"] if a["id"] == package["id"]]) == 0:
             print_new_package(package)
 
-    for package in release_packages["source"]:
-        if len([a for a in release_packages["destination"] if a["id"] == package["id"]]) == 0:
+    for package in release_packages["source"]["packages"]:
+        if len([a for a in release_packages["destination"]["packages"] if a["id"] == package["id"]]) == 0:
             print_removed_package(package)
 
 
 def download_packages(args, space_id, release_packages, path):
-    for package in release_packages["destination"]:
+    for package in release_packages["destination"]["packages"]:
         package["downloaded"] = download_package(args, space_id, package["id"], package["version"], path)
 
-    for package in release_packages["source"]:
-        matching = [a for a in release_packages["destination"] if
+    for package in release_packages["source"]["packages"]:
+        matching = [a for a in release_packages["destination"]["packages"] if
                     a["id"] == package["id"] and a["version"] == package["version"]]
         if len(matching) == 0:
             package["downloaded"] = download_package(args, space_id, package["id"], package["version"], path)
@@ -212,7 +266,7 @@ def extract_package(dir, archive):
 def extract_packages(release_packages_with_download):
     processed = {}
     for release_packages in release_packages_with_download.values():
-        for package in release_packages:
+        for package in release_packages["packages"]:
             if package["downloaded"] not in processed.keys():
                 package["extracted"] = extract_package(temp_dir, package["downloaded"])
                 processed[package["downloaded"]] = package["extracted"]
@@ -223,8 +277,8 @@ def extract_packages(release_packages_with_download):
 
 
 def compare_directories(release_packages_with_extract, left_only, right_only, diff):
-    for dest_package in release_packages_with_extract["destination"]:
-        for source_package in release_packages_with_extract["source"]:
+    for dest_package in release_packages_with_extract["destination"]["packages"]:
+        for source_package in release_packages_with_extract["source"]["packages"]:
             if dest_package["id"] == source_package["id"] and dest_package["version"] != source_package["version"]:
                 report = filecmp.dircmp(dest_package["extracted"], source_package["extracted"])
                 left_only(report.left_only, dest_package, source_package)
@@ -273,12 +327,23 @@ def print_changed_files(releases, files, dest_package, source_package):
                     print(line)
 
 
+def print_changed_step(release):
+    source_json = json.dumps(release["source"]["deployment_process"]["Steps"], indent=2)
+    dest_json = json.dumps(release["destination"]["deployment_process"]["Steps"], indent=2)
+
+    if source_json != dest_json:
+        diff = difflib.unified_diff(source_json.split('\n'), dest_json.split('\n'))
+        print("Changed detected in the deployment process:")
+        for line in diff:
+            print(line)
+
+
 args = get_args()
 space_id = space_name_to_id(args)
 project_id = project_name_to_id(args, space_id)
 releases = get_release(args, space_id, project_id)
 built_in_feed_id = get_built_in_feed_id(args, space_id)
-release_packages = get_release_packages(args, built_in_feed_id, space_id, releases)
+release_packages = flatten_release_with_packages_and_deployment(args, built_in_feed_id, space_id, releases)
 list_package_diff(release_packages,
                   lambda p: print("Release " + releases["destination"]["Version"]
                                   + " added the following package from release "
@@ -293,3 +358,4 @@ compare_directories(release_packages_with_extract,
                     lambda files, dest, source: print_added_files(releases, files, dest, source),
                     lambda files, dest, source: print_removed_files(releases, files, dest, source),
                     lambda files, dest, source: print_changed_files(releases, files, dest, source))
+print_changed_step(release_packages)
